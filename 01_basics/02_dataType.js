1b3f711cbba5640845ee103ce25bf5093afbb19b
Roshani159 <120788296+Roshani159@users.noreply.github.com>
2025-12-26 10:39:39 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,34 @@
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+3); 
+console.log("hitesh")  //code readability should be high
+
+
+
+
+
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+let state=null //undefined nahi khali hai
+
+    //   primitive datatype
+        //  ||
+        // number
+       // bigint
+       // string
+       // boolean=>true/false
+       //null- standalone value(khali value)
+       // undefined(value assign nahi hai)
+       // symbol(find uniqeness)
+
+
+    //    object
+
+
+    console.log(typeof "hitesh");//string
+    console.log(typeof null); //object
+    console.log(typeof undefined); //undefined
+    
+    
