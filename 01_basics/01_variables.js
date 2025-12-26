b814b7de44b482be0b0371be14c65c83e039582b
Roshani159 <120788296+Roshani159@users.noreply.github.com>
2025-12-26 10:11:28 +0000

    this is varible rule

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId_=144553  // not change value beacose it is constant
+
+
+// variable ko declare ke liye 2 way hai var and let but let jada use hota
+let accountEmail="roshanni@gemail.com"
+var accountPaddword="12345"
+accountCity="jaipur"  //good but not to say very good
+
+//accountId_=2 // NOT ALLOW
+console.log(accountId_);
+
+
+
+
+accountEmail="abc@.com"
+accountPaddword="8485"
+accountCity="jaoinpur"
+console.table([accountId_,accountEmail,accountPaddword,accountCity])
+
+
+
+//  prefer not use to var because of issue in block scope and functional scope
+
+
+let accountState;  // it give undefine value
+
+
